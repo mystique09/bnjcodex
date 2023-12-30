3bcf6ab5358e270a48f57fe71f785082f103a059
mystique09 <knightkrypto7@gmail.com>
2023-12-31 02:38:02 +0800

    chore: change metadata and background

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,15 +2,17 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Bnj G" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Benjie Ben Garcia" },
+    { name: "description", content: "Welcome to my portfolio." },
   ];
 };
 
 export default function Index() {
   return (
-    <div>
-      <h1 className="text-2xl">Welcome to Remix</h1>
-    </div>
+    <main className="h-[100svh]">
+      <section id="#" className="">
+        <h1 className="text-6xl">Welcome.</h1>
+      </section>
+    </main>
   );
 }
