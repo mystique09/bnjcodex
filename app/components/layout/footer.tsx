5680fcd5d223950a88ffd5c56153952b212df70c
mystique09 <knightkrypto7@gmail.com>
2023-12-31 13:31:33 +0800

    fix: footer links not clickable

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -5,10 +5,18 @@ export function Footer() {
         <div className="footer-wrapper max-w-6xl mx-auto py-5 flex items-start justify-between">
             <div className="flex flex-col items-start justify-center gap-2">
                 <h6 className="text-sm font-normal tracking-wider">Let's connect!</h6>
-                <div className="flex items-start justify-center gap-3">
-                    <MdiGithub className="size-10 fill-primary" />
-                    <MdiLinkedin className="size-10 fill-primary" />
-                </div>
+                <ul className="flex items-start justify-center gap-3">
+                    <li className="sm:group">
+                        <a aria-label="Github link" rel="noopener noreferrer" target="_blank" href="https://github.com/mystique09">
+                            <MdiGithub className="size-10 fill-primary sm:group-hover:fill-black sm:group-hover:scale-110  transition-all duration-300 ease-out" />
+                        </a>
+                    </li>
+                    <li className="sm:group">
+                        <a aria-label="Linkedin link" rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/benjie-ben-garcia-916261202/">
+                            <MdiLinkedin className="size-10 fill-primary sm:group-hover:fill-blue-600 sm:group-hover:scale-110  transition-all duration-300 ease-out" />
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </footer>
