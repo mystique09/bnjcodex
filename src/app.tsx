9c5800e395c720a2e10175b82190800721688287
mystique09 <knightkrypto7@gmail.com>
2024-01-10 02:00:50 +0800

    chore: format codebase

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,16 @@
 // @refresh reload
-import '@fontsource-variable/space-grotesk/wght.css';
-import '@fontsource/poppins/100.css';
-import '@fontsource/poppins/200.css';
-import '@fontsource/poppins/300.css';
-import '@fontsource/poppins/500.css';
-import '@fontsource/poppins/700.css';
+import "@fontsource-variable/space-grotesk/wght.css";
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start";
 import { Show, Suspense } from "solid-js";
 import "./app.css";
 
-import { MetaProvider } from '@solidjs/meta';
+import { MetaProvider } from "@solidjs/meta";
 import { Footer } from "./components/layout/footer";
 import { Header } from "./components/layout/header";
 
