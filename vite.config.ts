135be3a8782838fda018f7a00e323f1ab22fc4df
mystique09 <knightkrypto7@gmail.com>
2024-01-10 02:58:51 +0800

    add: cloudflare preset

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import cloudflare from "solid-start-cloudflare-workers";
-
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-	plugins: [cloudflare({ envPath: true })],
+	start: {
+		server: {
+			preset: "cloudflare-pages",
+		},
+	},
 });
